3f2d1db785a4abec27916109be2ec7ad33ecbfb1
Emiliano Perea <emilianoperea10@gmail.com>
2023-08-23 19:24:05 -0300

    fin de miniProyecto modal

--- Modal/Modal.js
+++ Modal/Modal.js
@@ -1,9 +1,15 @@
 const btn = document.getElementById("btn");
 const modal = document.getElementById("modal");
+const btnCerrar =document.getElementById("btn-cerrar")
 
 btn.addEventListener("click",()=>{
     modal.style.display="block";
     btn.style.display="none";
+})
+btnCerrar.addEventListener("click",()=>{
+    modal.style.display="none";
+    btn.style.display="block"
+
 })
 document.addEventListener("click", (event) => {
    
